82ad20a604bea9aa28f957b138776450d8c28f10
Hayley Tom <htom@crowdcomms.com.au>
2018-05-09 14:12:13 +1000

    Building CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 
 import { NgPokieComponent } from './ng-pokie.component.js';
+import './main.scss';
 
 export const NgPokieModule = angular.module('NgPokie', [])
     .component('ngPokie', NgPokieComponent)
